6d510ea30b45b9054fa45ca2016361f36e96e724
salmalifia05 <72839833+salmalifia05@users.noreply.github.com>
2020-12-19 11:01:36 +0700

    Add files via upload

---
+++ Sketch_Receiver_Sistem_Kerja_RF_Modem.ino
@@ -0,0 +1,23 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
+
+void setup() {
+ lcd.begin(20,4);
+ Serial.begin(9600);
+ lcd.setCursor(0,0);
+ lcd.print("Wireless Serial   ");
+ lcd.setCursor(0,1);
+ lcd.print("Wireless via 2,4 GHz");
+ lcd.setCursor (0,2);
+ lcd.print("Modem At PNJ ... ");
+}
+
+void loop() {
+  int TEMP_VALUE = Serial.read();
+  lcd.setCursor(0,3);
+  lcd.print("Temp. in 0C:");
+  lcd.setCursor(13,3);
+  lcd.print (TEMP_VALUE);
+  delay (100);
+  Serial.print ("TEMP_VAL: ");
+}
